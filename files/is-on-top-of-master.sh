b7ad5bb29e0e1f5f15745613b3ee8435a1305d0c
glx <glx@openttd.org>
2019-10-15 22:41:51 +0200

    Add: docker action

---
+++ files/is-on-top-of-master.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+TARGET_BRANCH=master
+if [ -n "${GITHUB_BASE_REF}" ]; then
+	TARGET_BRANCH=${GITHUB_BASE_REF}
+fi
+
+is_on_top_of_master=$(git branch $(git rev-parse --abbrev-ref HEAD) --contains origin/${TARGET_BRANCH} | wc -l)
+
+if [ "${is_on_top_of_master}" -ne 1 ]; then
+    echo "ERROR: This commit is not on top of ${TARGET_BRANCH}"
+    echo "(no further validation is done due to this error)"
+    exit 1
+fi
